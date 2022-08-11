a28d5c293bac67e764b22890929d5621ee8a5afb
drewgrif <drew@drewgriffin.com>
2022-08-11 15:01:35 -0400

    adding bspwm

---
+++ bspwm.sh
@@ -0,0 +1,108 @@
+ #!/bin/bash
+
+# Default packages are for the configuration and corresponding .config folders
+# Install packages after installing base Debian with no GUI
+
+# xorg display server installation
+sudo apt install -y xorg xbacklight xbindkeys xvkbd xinput
+
+# PACKAGE INCLUDES build-essential.
+sudo apt install -y build-essential
+
+# Microcode for Intel/AMD 
+# sudo apt install -y amd-microcode
+sudo apt install -y intel-microcode 
+
+# Network File Tools/System Events
+sudo apt install -y dialog mtools dosfstools avahi-daemon acpi acpid gvfs-backends
+
+sudo systemctl enable avahi-daemon
+sudo systemctl enable acpid
+
+# File Manager (eg. pcmanfm,krusader,thunar,nautilus)
+sudo apt install -y nautilus
+
+# Terminal (eg. terminator,kitty,xfce4-terminal)
+sudo apt install -y kitty
+
+# Sound packages
+sudo apt install -y pulseaudio alsa-utils pavucontrol volumeicon-alsa
+
+# Neofetch/HTOP
+sudo apt install -y neofetch htop
+
+# Network Manager
+sudo apt install -y network-manager network-manager-gnome 
+
+# Installation for Appearance management
+sudo apt install -y lxappearance 
+
+# Browser Installation (eg. chromium)
+sudo apt install -y firefox-esr 
+
+# Desktop background browser/handler 
+# feh --bg-fill /path/to/directory 
+# sudo apt install -y nitrogen 
+sudo apt install -y feh
+ 
+# Fonts and icons for now
+sudo apt install -y fonts-firacode fonts-liberation2 fonts-ubuntu papirus-icon-theme fonts-cascadia-code
+
+# EXA installation
+# replace ls command in .bashrc file with line below
+# alias ls='exa -al --long --header --color=always --group-directories-first' 
+sudo apt install -y exa
+
+# Printing and bluetooth (if needed)
+sudo apt install -y cups
+sudo apt install -y bluez blueman
+
+sudo systemctl enable bluetooth
+sudo systemctl enable cups
+
+
+# Packages needed i3-gaps after installation
+sudo apt install -y bspwm dmenu sxhkd numlockx rofi dunst libnotify-bin unzip geany simple-scan
+
+# Command line text editor -- nano preinstalled  -- I like micro but vim is great
+# sudo apt install -y micro
+sudo apt install -y vim
+
+# Install fonts
+sudo apt install fonts-font-awesome fonts-powerline fonts-ubuntu fonts-liberation2 fonts-liberation fonts-terminus
+
+# Create folders in user directory (eg. Documents,Downloads,etc.)
+xdg-user-dirs-update
+
+mkdir -p .config/{bspwm,sxhkd,dunst}
+
+install -Dm755 /usr/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm/bspwmrc
+install -Dm644 /usr/share/doc/bspwm/examples/sxhkdrc ~/.config/sxhkd/sxhkdrc
+
+# Required packages for ly console manager
+sudo apt install -y libpam0g-dev libxcb-xkb-dev
+
+# Install Ly Console Display Manager
+cd 
+cd Downloads
+git clone --recurse-submodules https://github.com/nullgemm/ly.git
+cd ly/
+make
+sudo make install
+sudo systemctl enable ly
+
+
+
+########################################################
+# End of script for default config
+#
+
+## These two scripts will install nerdfonts and copy my configuration files into the ~/.config directory
+## Configuration uses 
+
+source ~/i3gaps-debian/nerdfonts.sh
+source ~/i3gaps-debian/copyconf.sh
+
+sudo apt autoremove
+
+printf "\e[1;32mYou can now reboot! Thanks you.\e[0m\n"
